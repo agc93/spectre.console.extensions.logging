04bc86532c765b2e26777c424d61ad7e54b2dd1f
Alistair Chapman <alistair@agchapman.com>
2020-09-05 00:58:23 +1000

    Add examples

---
+++ samples/Default/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Default
+{
+    class Program
+    {
+        private static ILogger _logger;
+
+        static void Main(string[] args)
+        {
+            using (var factory = LoggerFactory.Create(b =>
+            {
+                b.SetMinimumLevel(LogLevel.Trace).AddSpectreConsole(c => { c.LogLevel = LogLevel.Trace; c.IncludeEventId = true; });
+            }))
+            {
+                _logger = factory.CreateLogger("SampleCategory");
+            }
+
+            _logger.LogInformation(0, "Sample application starting up...");
+            _logger.LogTrace(1234, "Use a {adjective} format for logging {noun}", "familiar", "messages");
+            _logger.LogWarning(100, "Your logs can have [bold italic]formatting[/]!");
+            _logger.LogDebug("This is doing well to not have [underline]any[/] critical errors");
+            _logger.LogCritical(500, "Log your errors with all the [bold underline white on red]impact[/] they deserve!");
+        }
+    }
+}
---
+++ samples/Inline/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Inline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = LoggerFactory.Create(b =>
+            {
+                b.SetMinimumLevel(LogLevel.Trace).AddInlineSpectreConsole(c => { c.LogLevel = LogLevel.Trace; });
+            });
+            var logger = factory.CreateLogger<Program>();
+
+            logger.LogInformation("Sample application starting up...");
+            logger.LogTrace("Use a {adjective} format for logging {noun}", "familiar", "messages");
+            logger.LogWarning("Your logs can have [bold italic]formatting[/]!");
+            logger.LogDebug("This is doing well to not have [underline]any[/] critical errors");
+            logger.LogCritical("Log your errors with all the [bold underline red on white]impact[/] they deserve!");
+        }
+    }
+}
